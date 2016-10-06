3555f8f70a3aecc98d9e67e849d89521682c938a
Alexander Winkler <winklera@ethz.ch>
2016-10-06 17:01:14 +0200

    added topic name declarations to be used in ros

---
+++ include/xpp_msgs/topic_names.h
@@ -0,0 +1,21 @@
+/**
+@file    topic_names.h
+@author  Alexander W. Winkler (winklera@ethz.ch)
+@date    Oct 5, 2016
+@brief   Brief description
+ */
+
+#ifndef USER_TASK_DEPENDS_XPP_MSGS_INCLUDE_XPP_MSGS_TOPIC_NAMES_H_
+#define USER_TASK_DEPENDS_XPP_MSGS_INCLUDE_XPP_MSGS_TOPIC_NAMES_H_
+
+namespace xpp_msgs {
+
+static const std::string start_walking_topic("xpp/start_walking");
+static const std::string goal_state_topic("xpp/goal_state");
+static const std::string req_info_nlp("required_info_nlp");
+static const std::string robot_trajectory("robot_trajectory");
+
+}
+
+
+#endif /* USER_TASK_DEPENDS_XPP_MSGS_INCLUDE_XPP_MSGS_TOPIC_NAMES_H_ */
