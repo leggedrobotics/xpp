237dade5237d0fa2e85b3aba939a5faa98e6b1cd
Alexander Winkler <winklera@ethz.ch>
2016-10-09 13:52:07 +0200

    added comments to topic names and "opt_variables_fixed"

--- include/xpp_msgs/topic_names.h
+++ include/xpp_msgs/topic_names.h
@@ -10,11 +10,23 @@
 
 namespace xpp_msgs {
 
+// command that tells the walking controller to start executing
 static const std::string start_walking_topic("xpp/start_walking");
+
+// position of the desired goal to reach
 static const std::string goal_state_topic("xpp/goal_state");
+
+// information necessary to setup an NLP
 static const std::string req_info_nlp("required_info_nlp");
+
+// sequence of full body hyq states as reference for a walking controller
 static const std::string robot_trajectory("robot_trajectory");
+
+// rviz marker topic for NLP's optimized variables (spline coefficients, footholds,...)
 static const std::string rviz_optimized("/optimization_variables");
+
+// rviz marker topic for fixed variables of NLP optimization (e.g. start stance)
+static const std::string rviz_fixed("optimization_fixed");
 
 }
 
